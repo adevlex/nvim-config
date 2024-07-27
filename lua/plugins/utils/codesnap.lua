30379a717307ab99c750410daffad57be1566a3d
Alex Diego <rosasrias82@gmail.com>
2024-07-27 14:16:42 -0500

    feat: add codesnap.nvim for code snippet screenshots

---
+++ lua/plugins/utils/codesnap.lua
@@ -0,0 +1,16 @@
+return
+{
+    'mistricky/codesnap.nvim',
+    build = 'make',
+    opts = {
+        save_path = "~/Pictures/",
+        mac_window_bar = true,
+        title = "CodeSnap",
+        code_font_family = "JetBrainsMono Nerd Font",
+        watermark_font_family = "Pacifico",
+        watermark = "Lexnyan",
+        bg_theme = "summer",
+        breadcrumbs_separator = "/",
+        has_line_number = true,
+    }
+}
