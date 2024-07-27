ef2bfcbaca8fb2555a461d4e63a9a3137ed36d07
Alex Diego <rosasrias82@gmail.com>
2024-07-27 14:24:28 -0500

    feat: add mason.nvim for LSP and DAP management

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,50 @@
+return
+{
+    "williamboman/mason.nvim",
+    lazy = false,
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+
+        local options = {
+            ui = {
+                icons = {
+                    package_pending = " ",
+                    package_installed = " ",
+                    package_uninstalled = " ",
+                },
+
+                keymaps = {
+                    toggle_server_expand = "<CR>",
+                    install_server = "i",
+                    update_server = "u",
+                    check_server_version = "c",
+                    update_all_servers = "U",
+                    check_outdated_servers = "C",
+                    uninstall_server = "X",
+                    cancel_installation = "<C-c>",
+                },
+            },
+
+            max_concurrent_installers = 10,
+        }
+
+        mason.setup(options)
+
+        require("mason-lspconfig").setup {
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "html",
+                "cssls",
+                "emmet_ls",
+                "pyright",
+                "clangd",
+                "omnisharp"
+            },
+        }
+    end
+}
