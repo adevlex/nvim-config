4b6b5c0746eb46aa9211ddcf418cc2e32facae77
Alex Diego <rosasrias82@gmail.com>
2024-07-27 14:22:44 -0500

    feat: add indent-blankline.nvim for indentation guides

---
+++ lua/plugins/ts/indent.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        event = { 'BufReadPost', 'BufNewFile' },
+        config = function()
+            local highlights = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+
+            require('ibl').setup {
+                indent = {
+                    highlight = highlights,
+                    char = "│",
+                    tab_char = "│",
+                },
+                scope = { enabled = false },
+                exclude = {
+                    filetypes = {
+                        "help",
+                        "alpha",
+                        "dashboard",
+                        "tedash",
+                        "neo-tree",
+                        "Trouble",
+                        "trouble",
+                        "lazy",
+                        "mason",
+                        "notify",
+                        "toggleterm",
+                        "lazyterm",
+                    },
+                },
+            }
+        end,
+    },
+
+    --    {
+    --        'echasnovski/mini.indentscope',
+    --        opts = { symbol = '│' },
+    --    },
+}
