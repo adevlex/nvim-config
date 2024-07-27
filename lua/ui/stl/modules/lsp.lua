e430c3054797fc6a4a90290947ebee8254f3b8f4
Alex Diego <rosasrias82@gmail.com>
2024-07-27 09:41:50 -0500

    feat: add module lsp for statusline

---
+++ lua/ui/stl/modules/lsp.lua
@@ -0,0 +1,21 @@
+-- thanks nvchad
+local M = function(m)
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+        if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+            if (m == 'blocks') then
+                return "%#StalineLspIcon#" ..
+                    " LSP " .. "%#StalineLspNameNormal#" .. " " .. client.name .. " %#StalineEmptySpace#" .. " "
+            elseif (m == 'minimal') then
+                return "%#StalineSep#| %#StalineEmptySpace#" ..
+                    "%#StalineLspNameMinimal#" .. client.name .. " %#StalineEmptySpace#"
+            elseif (m == 'fancy') then
+                return "%#StalineLspIcon#" ..
+                    "   " .. "%#StalineLspName#" .. " " .. client.name .. " " .. "%#StalineEmptySpace#" .. " "
+            else
+                return 'f'
+            end
+        end
+    end
+end
+
+return M
