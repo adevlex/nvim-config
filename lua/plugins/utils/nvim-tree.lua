9e4ecbac512db23b4a3fa734bbf927c9ab8e9a8e
Alex Diego <rosasrias82@gmail.com>
2024-07-27 10:46:03 -0500

    feat: add nvim-tree for file explorer
    Installed and configured nvim-tree to provide a file explorerwithin
    Neovim. This enhances navigation and file management

---
+++ lua/plugins/utils/nvim-tree.lua
@@ -0,0 +1,75 @@
+return
+{
+    'nvim-tree/nvim-tree.lua',
+    cmd = "NvimTreeToggle",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    keys = {
+        { "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
+    },
+    config = function()
+        require 'nvim-tree'.setup {
+            renderer = {
+                root_folder_label = false,
+                indent_markers = {
+                    enable = true,
+                },
+                icons = {
+                    glyphs = {
+                        default = ' ',
+                        symlink = ' ',
+                        git = {
+                            deleted = ' ',
+                            unstaged = ' ',
+                            untracked = ' ',
+                            staged = ' ',
+                            unmerged = ' ',
+                        }
+                    },
+                    show = {
+                        git = true,
+                        folder = true,
+                        file = true,
+                        folder_arrow = false,
+                    }
+                }
+            },
+            actions = {
+                open_file = {
+                    window_picker = {
+                        exclude = {
+                            filetype = {
+                                "packer",
+                                "qf"
+                            },
+                            buftype = {
+                                "terminal",
+                                "help"
+                            }
+                        },
+                    },
+                },
+            },
+            filters = {
+                exclude = { '.git', 'node_modules', '.cache' },
+            },
+            update_focused_file = {
+                enable = true,
+                update_root = true,
+            },
+            git = {
+                enable = true,
+                ignore = false,
+                timeout = 500,
+            },
+            hijack_directories = { enable = true },
+            view = {
+                width = 30,
+                side = "left",
+            },
+        }
+        vim.cmd [[
+autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+nnoremap <leader>cd :cd %:p:h<CR>:pwd<CR>
+]]
+    end
+}
