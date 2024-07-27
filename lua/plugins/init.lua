1a92f9fe79078f05dc98f056fb592ab893aa6c9e
Alex Diego <rosasrias82@gmail.com>
2024-07-27 10:16:25 -0500

    feat: add Lazy.nvim as plugin manager
    Installed and configured Lazy.nvim to manage plugins in Neovim. This
    setup improves plugin management and loading efficiency

---
+++ lua/plugins/init.lua
@@ -0,0 +1,45 @@
+local function clone_lazy_nvim_repo()
+    local lazy_plugin_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+    if vim.loop.fs_stat(lazy_plugin_path) then
+        return
+    end
+
+    local git_clone_cmd = { 'git', 'clone', 'https://github.com/folke/lazy.nvim.git', lazy_plugin_path }
+    local result = vim.fn.system(git_clone_cmd)
+
+    if vim.v.shell_error ~= 0 then
+        error("Error al clonar el repositorio lazy.nvim: " .. result)
+    end
+end
+
+function Bootstrap()
+    clone_lazy_nvim_repo()
+
+    local lazy_plugin_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+    vim.opt.rtp:prepend(lazy_plugin_path)
+end
+
+function Setup()
+    local status_ok, lazy = pcall(require, 'lazy')
+
+    if not status_ok then
+        error("No se pudo cargar el módulo 'lazy'")
+    end
+
+    lazy.setup({
+        spec = {
+            { import = 'plugins.utils' },
+            { import = 'plugins.lsp' },
+            { import = 'plugins.comment' },
+            { import = 'plugins.dap' },
+            { import = 'plugins.git' },
+            { import = 'plugins.ia' },
+            { import = 'plugins.ts' },
+            { import = 'plugins.utils' }
+        },
+    })
+end
+
+Bootstrap()
+Setup()
