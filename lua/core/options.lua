897f944c6aa7ebcd8dd6d0fadfdf74426170ab2a
Alex Diego <rosasrias82@gmail.com>
2024-07-26 20:30:14 -0500

    feat: add options.lua for a basic configuration for Neovim

---
+++ lua/core/options.lua
@@ -0,0 +1,65 @@
+-- NOTE: Neovim options
+
+vim.loader.enable()
+--local config = require("core.cfg")
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
+o.laststatus = 3
+o.showmode = false
+o.wrap = false
+
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.cursorlineopt = "number"
+
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 4
+o.smartindent = true
+o.tabstop = 4
+o.softtabstop = 4
+
+opt.fillchars = { eob = " " }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+
+-- Numbers
+o.number = true
+o.numberwidth = 2
+o.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+o.undofile = true
+o.termguicolors = true
+o.cmdheight = 0
+
+-- interval for writing swap file to disk, also used by gitsigns
+o.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+--opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }
+
+-- disable some default providers
+g["loaded_node_provider"] = 0
+g["loaded_python3_provider"] = 0
+g["loaded_perl_provider"] = 0
+g["loaded_ruby_provider"] = 0
+
+g.toggle_cmp = true
+g.code_action_menu_window_border = 'single'
+g.NvimTheme = "catppuccin"
+g.themeCache = vim.fn.stdpath "data" .. "/colors_data/"
+g.transparency = true
+g.loadNvimTheme = true
+g.statusStyle = 'fancy' -- fancy, minimal, blocks
