a3306101df874d9a2eddedf8f10a04337b85bc8b
Alex Diego <rosasrias82@gmail.com>
2024-07-27 14:26:42 -0500

    feat: add nvim-autopairs for automatic pair insertion

---
+++ lua/plugins/lsp/autopair.lua
@@ -0,0 +1,16 @@
+return
+{
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {
+        fast_wrap = {},
+        disable_filetype = { 'TelescopePrompt', 'Vim' },
+    },
+    config = function(_, opts)
+        require("nvim-autopairs").setup(opts)
+
+        -- setup cmp for autopairs
+        local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+        require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+}
