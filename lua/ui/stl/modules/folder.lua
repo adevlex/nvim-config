39b6ea40bfe61b73bc13d4ed35e64f6e49160531
Alex Diego <rosasrias82@gmail.com>
2024-07-27 09:41:37 -0500

    feat: add module folder for statusline

---
+++ lua/ui/stl/modules/folder.lua
@@ -0,0 +1,19 @@
+local fn = vim.fn
+local M = function(m)
+    if (m == 'blocks') then
+        local directory = "%#StalineFolderText#" .. " " .. fn.fnamemodify(fn.getcwd(), ":t") .. " "
+        local icon = "%#StalineFolderIcon#" .. " DIR "
+        return icon .. directory .. "%#StalineEmptySpace#" .. " "
+    elseif (m == 'fancy') then
+        local directory = "%#StalineFolderText#" .. " " .. fn.fnamemodify(fn.getcwd(), ":t") .. " "
+        local icon = "%#StalineFolderIcon#" .. "  "
+        return "%#StalineFolderSep#" .. "" .. icon .. directory .. "%#StalineEmptySpace#" .. " "
+    elseif (m == 'minimal') then
+        local directory = "%#StalineFolderTextMinimal#" .. " " .. fn.fnamemodify(fn.getcwd(), ":t") .. " "
+        return directory .. "%#StalineEmptySpace#"
+    else
+        return 'f'
+    end
+end
+
+return M
