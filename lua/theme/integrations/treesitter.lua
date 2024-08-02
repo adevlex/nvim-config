054320abca5eae6f900f7e1e0f8b07914054fd54
Alex Diego <rosasrias82@gmail.com>
2024-08-01 22:41:59 -0500

    Update variable color

--- lua/theme/integrations/treesitter.lua
+++ lua/theme/integrations/treesitter.lua
@@ -48,7 +48,7 @@ return {
     ["@type.builtin"] = { fg = colors.base0A },
     ["@number.float"] = { fg = colors.base09 },
 
-    ["@variable"] = { fg = colors.base09 },
+    ["@variable"] = { fg = colors.base05 },
     ["@variable.builtin"] = { fg = colors.base09 },
     ["@variable.member"] = { fg = colors.base08 },
     ["@variable.member.key"] = { fg = colors.base08 },
