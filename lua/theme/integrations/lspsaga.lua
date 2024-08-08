c4cf1f3ffcc01840c4c3195d47194e29da54118e
Alex Diego <rosasrias82@gmail.com>
2024-08-08 14:59:23 -0500

    update: border lspsaga

--- lua/theme/integrations/lspsaga.lua
+++ lua/theme/integrations/lspsaga.lua
@@ -6,7 +6,7 @@ end
 
 return {
     SagaNormal = { bg = colors.darker },
-    -- SagaBorder = { bg = colors.darker, fg = colors.darker },
+    SagaBorder = { bg = colors.darker, fg = colors.darker },
     RenameNormal = { bg = colors.darker },
-    -- RenameBorder = { bg = colors.darker, fg = colors.darker },
+    RenameBorder = { bg = colors.darker, fg = colors.darker },
 }
