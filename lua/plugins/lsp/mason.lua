1f096c924dfcb4ed8acd860b062f00337b270016
Alex <rosasrias82@gmail.com>
2024-09-18 18:47:32 -0500

    Update tsserver to ts_ls

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -37,7 +37,7 @@ return
         require("mason-lspconfig").setup {
             ensure_installed = {
                 "lua_ls",
-                "tsserver",
+                "ts_ls",
                 "html",
                 "cssls",
                 "emmet_ls",
