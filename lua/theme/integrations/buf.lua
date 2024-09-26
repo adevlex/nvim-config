1eb5642bcc7f4b922dbe075cc39b3b2cfa7ced29
Alex <rosasrias82@gmail.com>
2024-09-25 21:13:12 -0500

    Delete old bufferline hl

--- lua/theme/integrations/buf.lua
+++ lua/theme/integrations/buf.lua
@@ -21,13 +21,4 @@ return {
     BuffLineRun = { bg = utils.blend(colors.base0F, colors.base00, 0.1), fg = colors.base0F },
     BuffLineSplit = { bg = utils.blend(colors.base0B, colors.base00, 0.1), fg = colors.base0B },
     BufflineTrans = { bg = utils.blend(colors.base0D, colors.base00, 0.1), fg = colors.base0D },
-
-    -- OLD BUFFERLINE HIGHLIGHTS
-
-    --BufflineBufOnActive = { bg = colors.base09, fg = colors.base00 },
-    --BufflineBufOnInactive = { fg = colors.base07, bg = colors.base00 },
-    --BuffLineBufOnModified = { bg = colors.base09, fg = colors.base00 },
-    --BuffLineBufOffModified = { fg = colors.base0F, bg = colors.base00 },
-    --BuffLineBufOnClose = { bg = colors.base09, fg = colors.base00 },
-    --BuffLineBufOffClose = { fg = colors.base0F, bg = utils.blend(colors.base0F, colors.base00, 0.1) },
 }
