e164eab0262e0a4ceb230e5ef9cfb5f557d38a5d
Alex <rosasrias82@gmail.com>
2024-10-02 19:27:06 -0500

    Change kind_icons

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -15,41 +15,41 @@ return {
         end
 
         local kind_icons = {
-            Array = " 󰅨 ",
-            Boolean = "  ",
-            Class = " 󰠱 ",
-            Color = "  ",
-            Constant = " 󰐀 ",
-            Constructor = " 󰆦 ",
-            Enum = " 󰎡 ",
-            EnumMember = "   ",
-            Event = " 󱕍 ",
-            Field = " 󰋒 ",
-            File = "  ",
-            Folder = " 󰉋 ",
-            Function = " 󰬍 ",
-            Interface = "  ",
-            Key = " 󰃽 ",
-            Keyword = " 󰢬 ",
-            Method = " 󱩗 ",
-            Module = " 󰕳 ",
-            Namespace = " 󰀆 ",
-            Null = " 󰛎 ",
-            Number = " 󰠾 ",
-            Object = " 󰋌 ",
-            Operator = " 󰿉 ",
-            Package = "  ",
-            Property = "  ",
-            Reference = " 󱎙 ",
-            Snippet = " 󰏬 ",
-            String = " 󰠹 ",
-            Struct = " 󰾔 ",
-            Text = " 󰏬 ",
-            TypeParameter = " 󰒔 ",
-            Unit = " 󰻬 ",
-            Value = " 󱊩 ",
-            Variable = " 󰜵 ",
-            Codeium = " 󱎙 ",
+            Array = " 󰅪 ",
+            Boolean = " 󰡘 ",
+            Class = " 󱓑 ",
+            Color = " 󰸌 ",
+            Constant = " 󱡠 ",
+            Constructor = " 󰆣 ",
+            Enum = " 󰉛 ",
+            EnumMember = " 󰐂 ",
+            Event = " 󱓷 ",
+            Field = " 󰛻 ",
+            File = " 󰷪 ",
+            Folder = " 󰉑 ",
+            Function = " 󰡴 ",
+            Interface = " 󰧞 ",
+            Key = " 󰌠 ",
+            Keyword = " 󰉠 ",
+            Method = " 󰟔 ",
+            Module = " 󰏦 ",
+            Namespace = " 󰅗 ",
+            Null = " 󰠗 ",
+            Number = " 󰎔 ",
+            Object = " 󰠌 ",
+            Operator = " 󰆽 ",
+            Package = " 󰒒 ",
+            Property = " 󰜢 ",
+            Reference = " 󰛉 ",
+            Snippet = " 󱋖 ",
+            String = " 󰉾 ",
+            Struct = " 󱓟 ",
+            Text = " 󰉡 ",
+            TypeParameter = " 󰒅 ",
+            Unit = " 󰣺 ",
+            Value = " 󱈄 ",
+            Variable = " 󰡧 ",
+            Codeium = " 󱓑 ",
         }
 
         cmp.setup({
@@ -101,14 +101,40 @@ return {
                 fields = { 'kind', 'abbr', 'menu' },
                 format = function(entry, vim_item)
                     vim_item.kind = kind_icons[vim_item.kind]
+
+                    local colors = require("theme").getCurrentTheme()
+                    local utils = require("core.utils")
+
+                    if not colors then
+                        return { error = "colors not found" }
+                    end
+
+                    local menu_colors = {
+                        buffer = { fg = colors.base0C, bg = utils.blend(colors.base03, colors.darker, 0.3) },   -- Celeste sólido y fondo oscuro
+                        path = { fg = colors.base0A, bg = utils.blend(colors.base02, colors.darker, 0.3) },     -- Amarillo sólido
+                        nvim_lsp = { fg = colors.base09, bg = utils.blend(colors.base03, colors.darker, 0.3) }, -- Naranja sólido
+                        luasnip = { fg = colors.base0D, bg = utils.blend(colors.base04, colors.darker, 0.3) },  -- Azul sólido
+                        cmdline = { fg = colors.base0E, bg = utils.blend(colors.base05, colors.darker, 0.3) },  -- Magenta sólido
+                        nvim_lua = { fg = colors.base0B, bg = utils.blend(colors.base06, colors.darker, 0.3) }, -- Verde sólido
+                        codeium = { fg = colors.base08, bg = utils.blend(colors.base02, colors.darker, 0.3) },  -- Rojo sólido
+                    }
+
+                    -- Aplicar colores de fg y bg
+                    local current_menu = menu_colors[entry.source.name] or { fg = colors.base05, bg = colors.darker }
+                    vim_item.menu = string.format("%%#CmpItemMenu# %s", vim_item.menu)
+
+                    -- Definir el highlight para el menú dinámicamente
+                    vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = current_menu.fg, bg = current_menu.bg })
+
+
                     vim_item.menu = ({
-                        buffer = "󰑬 [Buf]",
-                        path = "󰿠 [Pat]",
-                        nvim_lsp = "󰒔 [Lsp]",
-                        luasnip = "󰢑 [Snp]",
-                        cmdline = " [Cmd]",
-                        nvim_lua = " [Lua]",
-                        codeium = "󱎙 [Aic]",
+                        buffer = "󰈔  [Buf]",
+                        path = "󰉖  [Pat]",
+                        nvim_lsp = "󰒕  [Lsp]",
+                        luasnip = "󰜷  [Snp]",
+                        cmdline = "  [Cmd]",
+                        nvim_lua = "  [Lua]",
+                        codeium = "󱎑  [Aic]",
                     })[entry.source.name]
                     return vim_item
                 end,
