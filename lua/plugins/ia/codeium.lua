bb6ce5bf7f3ab90b54b7226bb21aa8d2ed00e8b2
Alex <rosasrias82@gmail.com>
2024-10-04 09:31:40 -0500

    Update codeium config

--- lua/plugins/ia/codeium.lua
+++ lua/plugins/ia/codeium.lua
@@ -5,6 +5,8 @@ return {
         "hrsh7th/nvim-cmp",
     },
     config = function()
-        require("codeium").setup({})
+        require("codeium").setup({
+            timeout_ms = 1000,
+        })
     end
 }
