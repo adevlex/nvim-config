cbf62a1945465d263c80ec114e2909700f5f962e
Alex <rosasrias82@gmail.com>
2024-10-04 17:27:32 -0500

    Add pluggin colorify author NvChad

---
+++ lua/colorify/init.lua
@@ -0,0 +1,35 @@
+local M = {}
+local api = vim.api
+
+local state = require("colorify.state")
+state.ns = api.nvim_create_namespace "Colorify"
+api.nvim_set_hl_ns(state.ns)
+
+M.attach = require("colorify.attach")
+
+M.run = function()
+    api.nvim_create_autocmd({
+        "TextChanged",
+        "TextChangedI",
+        "TextChangedP",
+        "VimResized",
+        "LspAttach",
+        "WinScrolled",
+        "BufEnter",
+    }, {
+        callback = function(args)
+            local tblen = #state.events
+            if state.events[tblen] == "BufEnter" and args.event == "WinScrolled" then
+                return
+            end
+
+            table.insert(state.events, args.event)
+
+            if vim.bo[args.buf].bl then
+                M.attach(args.buf, args.event)
+            end
+        end,
+    })
+end
+
+return M
