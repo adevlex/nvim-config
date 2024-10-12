726b5f11e7dedb1f59bbed5f92a923c8dfe64981
Alex <rosasrias82@gmail.com>
2024-10-12 16:34:01 -0500

    Update settings keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,15 +19,15 @@ function ClickGit()
     lazygit:toggle()
 end
 
--- local function map(mode, lhs, rhs, opts)
---     local options = { noremap = true, silent = true }
---     if opts then
---         options = vim.tbl_extend("force", options, opts)
---     end
---     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
--- end
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 
-local map = vim.keymap.set
+-- local map = vim.keymap.set
 
 vim.g.mapleader = " "
 
