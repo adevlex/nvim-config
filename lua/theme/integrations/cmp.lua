af1d1b2c8dd47916ac72877d22ea60c87756a09c
Alex Rosas <rosasrias82@gmail.com>
2024-12-15 18:31:22 -0500

    Update hl

--- lua/theme/integrations/cmp.lua
+++ lua/theme/integrations/cmp.lua
@@ -9,12 +9,6 @@ local function createAlphaBg(foreground, alpha)
     return utils.blend(foreground, colors.background, alpha)
 end
 
--- Mezclar colores usando 'mix'
-local function mixColors(c1, c2, weight)
-    return utils.mix(c1, c2, weight)
-end
-
-
 return {
     -- Aquí utilizo 'foreground' sólido y 'background' con alpha para el contraste
     CmpItemKindArray = { fg = colors.base0B, bg = createAlphaBg(colors.base0B, 0.2) },
@@ -56,13 +50,14 @@ return {
 
     -- Otros elementos del cmp
     CmpItemAbbr = { fg = colors.foreground },
-    CmpItemAbbrDeprecated = { fg = colors.base0B },
-    CmpItemAbbrMatch = { fg = colors.base05 },
-    CmpItemAbbrMatchFuzzy = { fg = colors.base06 },
+    CmpItemAbbrDeprecated = { fg = colors.base09 },
+    CmpItemAbbrMatch = { fg = colors.base0B },
+    CmpItemAbbrMatchFuzzy = { fg = colors.base08 },
     CmpItemKind = { fg = colors.foreground },
     CmpItemMenu = { fg = colors.base0C },
     CmpDoc = { bg = colors.lighter },
-    Pmenu = { fg = colors.foreground, bg = colors.darker },
+    Pmenu = { fg = colors.base05, bg = colors.darker },
+    CmpBorder = { fg = colors.base04, bg = "NONE" },
     PmenuSel = { bg = utils.blend(colors.base0D, colors.darker, 0.4) },
     PmenuSbar = { bg = colors.darker },
     PmenuThumb = { bg = colors.base0B },
