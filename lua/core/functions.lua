c04aa86bacb05d03cc27f4284dfb48f7ed35c57e
Alex Rosas <rosasrias82@gmail.com>
2024-12-15 18:19:42 -0500

    Fix format and add run languages

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -1,19 +1,23 @@
+-- Módulo principal
 local M = {}
 
+-- Notificación utilitaria
 local function notify(message, level, options)
     vim.notify(message, level or vim.log.levels.INFO, options)
 end
 
+-- Sustitución de cadenas en comandos
 local function substitute(cmd)
-    return cmd:gsub("%%", vim.fn.expand "%")
-        :gsub("$fileBase", vim.fn.expand "%:r")
-        :gsub("$filePath", vim.fn.expand "%:p")
-        :gsub("$file", vim.fn.expand "%")
-        :gsub("$dir", vim.fn.expand "%:p:h")
-        :gsub("#", vim.fn.expand "#")
-        :gsub("$altFile", vim.fn.expand "#")
+    return cmd:gsub("%%", vim.fn.expand("%"))
+        :gsub("$fileBase", vim.fn.expand("%:r"))
+        :gsub("$filePath", vim.fn.expand("%:p"))
+        :gsub("$file", vim.fn.expand("%"))
+        :gsub("$dir", vim.fn.expand("%:p:h"))
+        :gsub("#", vim.fn.expand("#"))
+        :gsub("$altFile", vim.fn.expand("#"))
 end
 
+-- Funciones para alternar configuraciones
 local function toggle_option(option)
     local value = not vim.api.nvim_get_option_value(option, {})
     vim.opt[option] = value
@@ -27,27 +31,29 @@ local function toggle_tabline()
     notify("showtabline set to " .. tostring(value))
 end
 
+-- Función principal para construir y ejecutar comandos según el tipo de archivo
 local function build_run()
-    local file_extension = vim.fn.expand "%:e"
+    local file_extension = vim.fn.expand("%:e")
     local term_cmd = "bot 10 new | term "
 
+    -- Tabla de comandos soportados por extensión de archivo
     local supported_filetypes = {
         html = { default = "live-server ." },
         c = { default = "gcc % -o $fileBase && ./$fileBase", debug = "gcc -g % -o $fileBase && $fileBase" },
-        cs = { default = "dotnet run" },
-        cpp = { default = "g++ % -o  $fileBase && ./$fileBase", debug = "g++ -g % -o  $fileBase", competitive = "g++ -std=c++17 -Wall -DAL -O2 % -o $fileBase && $fileBase" },
+        cpp = {
+            default = "g++ % -o $fileBase && ./$fileBase",
+            debug = "g++ -g % -o $fileBase",
+            competitive = "g++ -std=c++17 -Wall -DAL -O2 % -o $fileBase && $fileBase",
+        },
         py = { default = "python %" },
-        go = { default = "go run %" },
-        java = { default = "java %" },
         js = { default = "node %", debug = "node --inspect %" },
         ts = { default = "tsc % && node $fileBase" },
         rs = { default = "rustc % && $fileBase" },
-        php = { default = "php %" },
-        r = { default = "Rscript %" },
-        jl = { default = "julia %" },
-        rb = { default = "ruby %" },
-        pl = { default = "perl %" },
-        tex = { default = "pdflatex %" },
+        cs = { default = "dotnet run %" },
+        lua = { default = "lua %" },
+        java = { default = "javac % && java $fileBase" },
+        go = { default = "go run %" },
+        -- Agrega más tipos según sea necesario
     }
 
     local function execute_command(cmd)
@@ -60,11 +66,6 @@ local function build_run()
     end
 
     local options = vim.tbl_keys(supported_filetypes[file_extension])
-    if #options == 0 then
-        notify("It doesn't contain any command", vim.log.levels.WARN, { title = "Code Runner" })
-        return
-    end
-
     if #options == 1 then
         execute_command(supported_filetypes[file_extension][options[1]])
         return
@@ -78,6 +79,7 @@ local function build_run()
     end)
 end
 
+-- Alternar la terminal LazyGit
 local function lazygit_toggle()
     local lazygit = require("toggleterm.terminal").Terminal:new({
         cmd = "lazygit",
@@ -92,6 +94,7 @@ local function lazygit_toggle()
     lazygit:toggle()
 end
 
+-- Alternar la terminal Ranger
 local function ranger_toggle()
     if vim.fn.executable("ranger") == 0 then
         notify("ranger isn't installed")
@@ -108,6 +111,7 @@ local function ranger_toggle()
     ranger:toggle()
 end
 
+-- Exportar funciones
 M.toggle_option = toggle_option
 M.toggle_tabline = toggle_tabline
 M.build_run = build_run
