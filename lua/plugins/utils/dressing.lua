1d37dc42ab325f6c19596e5a6ffa5e88f955003f
Alex Rosas <rosasrias82@gmail.com>
2024-12-15 18:29:21 -0500

    Fix format

--- lua/plugins/utils/dressing.lua
+++ lua/plugins/utils/dressing.lua
@@ -1,26 +1,4 @@
-return
-{
+return {
     'stevearc/dressing.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = 'rcarriga/nvim-notify',
-    config = function()
-        require("dressing").setup({
-            input = {
-                enabled = true,
-                default_prompt = "➤ ",
-                win_options = {
-                    winblend = 10,
-                },
-            },
-            select = {
-                enabled = true,
-                backend = { "telescope", "buildtin" },
-                buildtin = {
-                    win_options = {
-                        winblend = 0,
-                    },
-                },
-            },
-        })
-    end,
+    opts = {},
 }
