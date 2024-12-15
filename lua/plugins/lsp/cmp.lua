e7ff013dd63f3b921e509a9debad96c474021e0c
Alex Rosas <rosasrias82@gmail.com>
2024-12-15 18:25:45 -0500

    Update lspkind icons and fix format

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,55 +1,55 @@
 return {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
     },
     config = function()
-        local present, cmp = pcall(require, 'cmp')
+        local present, cmp = pcall(require, "cmp")
         if not present then
             return
         end
 
         local kind_icons = {
-            Array = " 󰅪 ",
-            Boolean = " 󰡘 ",
-            Class = " 󱓑 ",
-            Color = " 󰸌 ",
-            Constant = " 󱡠 ",
-            Constructor = " 󰆣 ",
-            Enum = " 󰉛 ",
-            EnumMember = " 󰐂 ",
-            Event = " 󱓷 ",
-            Field = " 󰛻 ",
-            File = " 󰷪 ",
-            Folder = " 󰉑 ",
-            Function = " 󰡴 ",
-            Interface = " 󰧞 ",
-            Key = " 󰌠 ",
-            Keyword = " 󰉠 ",
-            Method = " 󰟔 ",
-            Module = " 󰏦 ",
-            Namespace = " 󰅗 ",
-            Null = " 󰠗 ",
-            Number = " 󰎔 ",
-            Object = " 󰠌 ",
-            Operator = " 󰆽 ",
-            Package = " 󰒒 ",
-            Property = " 󰜢 ",
-            Reference = " 󰛉 ",
-            Snippet = " 󱋖 ",
-            String = " 󰉾 ",
-            Struct = " 󱓟 ",
-            Text = " 󰉡 ",
-            TypeParameter = " 󰒅 ",
-            Unit = " 󰣺 ",
-            Value = " 󱈄 ",
-            Variable = " 󰡧 ",
-            Supermaven = "  ",
+            Array = " 󰅪 ", -- Representa una lista o conjunto de datos
+            Boolean = " 󰨊 ", -- Booleanos
+            Class = " 󰠱 ", -- Clases
+            Color = " 󰏘 ", -- Representación de colores
+            Constant = " 󰏿 ", -- Constantes
+            Constructor = " 󰆧 ", -- Constructores
+            Enum = " 󰕘 ", -- Enumeraciones
+            EnumMember = " 󰕣 ", -- Miembros de enumeraciones
+            Event = " 󰘉 ", -- Eventos
+            Field = " 󰇾 ", -- Campos
+            File = " 󰈙 ", -- Archivos
+            Folder = " 󰉋 ", -- Carpetas
+            Function = " 󰊕 ", -- Funciones
+            Interface = " 󰋱 ", -- Interfaces
+            Key = " 󰌋 ", -- Claves
+            Keyword = " 󰇠 ", -- Palabras clave
+            Method = " 󰊕 ", -- Métodos
+            Module = " 󰌗 ", -- Módulos
+            Namespace = " 󰅪 ", -- Espacios de nombres
+            Null = " 󰟢 ", -- Valor nulo
+            Number = " 󰉹 ", -- Números
+            Object = " 󰇧 ", -- Objetos
+            Operator = " 󰆕 ", -- Operadores
+            Package = " 󰏓 ", -- Paquetes
+            Property = " 󰓹 ", -- Propiedades
+            Reference = " 󰦪 ", -- Referencias
+            Snippet = " 󰲡 ", -- Fragmentos
+            String = " 󰀬 ", -- Cadenas
+            Struct = " 󰖸 ", -- Estructuras
+            Text = " 󰈶 ", -- Texto
+            TypeParameter = " 󰗵 ", -- Parámetros de tipo
+            Unit = " 󰑑 ", -- Unidades
+            Value = " 󰠡 ", -- Valores
+            Variable = " 󰡦 ", -- Variables
+            Supermaven = " 󰗚 ", -- Icono especial para personalización
         }
 
         cmp.setup({
@@ -64,21 +64,20 @@ return {
 
             window = {
                 completion = {
-                    border = "solid",
+                    border = "rounded",
                     scrollbar = false,
-                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None"
+                    winhighlight = "Normal:Pmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
                 },
                 documentation = {
-                    border = "solid",
+                    border = "rounded",
                     scrollbar = false,
-                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None"
-                }
+                    winhighlight = "Normal:Pmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+                },
             },
-
             mapping = cmp.mapping.preset.insert({
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<Esc>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Esc>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
@@ -94,47 +93,20 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-
             }),
 
             formatting = {
-                fields = { 'kind', 'abbr', 'menu' },
+                fields = { "kind", "abbr", "menu" },
                 format = function(entry, vim_item)
                     vim_item.kind = kind_icons[vim_item.kind]
-
-                    local colors = require("theme").getCurrentTheme()
-                    local utils = require("core.utils")
-
-                    if not colors then
-                        return { error = "colors not found" }
-                    end
-
-                    local menu_colors = {
-                        buffer = { fg = colors.base0C, bg = utils.blend(colors.base03, colors.darker, 0.3) },   -- Celeste sólido y fondo oscuro
-                        path = { fg = colors.base0A, bg = utils.blend(colors.base02, colors.darker, 0.3) },     -- Amarillo sólido
-                        nvim_lsp = { fg = colors.base09, bg = utils.blend(colors.base03, colors.darker, 0.3) }, -- Naranja sólido
-                        luasnip = { fg = colors.base0D, bg = utils.blend(colors.base04, colors.darker, 0.3) },  -- Azul sólido
-                        cmdline = { fg = colors.base0E, bg = utils.blend(colors.base05, colors.darker, 0.3) },  -- Magenta sólido
-                        nvim_lua = { fg = colors.base0B, bg = utils.blend(colors.base06, colors.darker, 0.3) }, -- Verde sólido
-                        codeium = { fg = colors.base08, bg = utils.blend(colors.base02, colors.darker, 0.3) },  -- Rojo sólido
-                    }
-
-                    -- Aplicar colores de fg y bg
-                    local current_menu = menu_colors[entry.source.name] or { fg = colors.base05, bg = colors.darker }
-                    vim_item.menu = string.format("%%#CmpItemMenu# %s", vim_item.menu)
-
-                    -- Definir el highlight para el menú dinámicamente
-                    vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = current_menu.fg, bg = current_menu.bg })
-
-
                     vim_item.menu = ({
-                        buffer = "󰈔  [Buf]",
-                        path = "󰉖  [Pat]",
-                        nvim_lsp = "󰒕  [Lsp]",
-                        luasnip = "󰜷  [Snp]",
-                        cmdline = "  [Cmd]",
-                        nvim_lua = "  [Lua]",
-                        supermaven = "󱎑  [Aic]",
+                        buffer = "󰈔  Buf",
+                        path = "󰉖  Pat",
+                        nvim_lsp = "󰒕  Lsp",
+                        luasnip = "󰜷  Snp",
+                        cmdline = "  Cmd",
+                        nvim_lua = "  Lua",
+                        supermaven = "󱎑  Aic",
                     })[entry.source.name]
 
                     local api = vim.api
@@ -144,7 +116,7 @@ return {
                         format_colors = {
                             tailwind = false,
                             icon = "󱓻 ",
-                        }
+                        },
                     }
 
                     local colors_icon = "󱓻 " .. " "
@@ -153,7 +125,7 @@ return {
                         local entryItem = entr:get_completion_item()
                         local color = entryItem.documentation
 
-                        if color and type(color) == "string" and color:match "^#%x%x%x%x%x%x$" then
+                        if color and type(color) == "string" and color:match("^#%x%x%x%x%x%x$") then
                             local hl = "hex-" .. color:sub(2)
 
                             if #api.nvim_get_hl(0, { name = hl }) == 0 then
@@ -185,5 +157,5 @@ return {
                 { name = "supermaven" },
             }),
         })
-    end
+    end,
 }
