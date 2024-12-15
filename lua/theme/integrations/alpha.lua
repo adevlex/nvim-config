21dda75e346d5c8c2185fe5272a35977d7551c56
Alex Rosas <rosasrias82@gmail.com>
2024-12-15 18:31:15 -0500

    Update hl

--- lua/theme/integrations/alpha.lua
+++ lua/theme/integrations/alpha.lua
@@ -2,14 +2,14 @@ local colors = require("theme").getCurrentTheme()
 local utils = require("core.utils")
 
 if not colors then
-    return { error = "colors not found" }
+	return { error = "colors not found" }
 end
 
 return {
-    AlphaHeader = { fg = colors.base0D, bg = colors.background },
-    AlphaLabel = { fg = colors.base07, bg = colors.background },
-    AlphaIcon = { fg = colors.base0D, bold = true },
-    AlphaKeyPrefix = { fg = colors.red, bg = utils.blend(colors.red, colors.base00, 0.1) },
-    AlphaMessage = { fg = colors.base0D, bg = colors.background },
-    AlphaFooter = { fg = colors.base0A, bg = colors.background },
+	AlphaHeader = { fg = colors.base0E, bg = colors.background },
+	AlphaLabel = { fg = colors.base09, bg = colors.background },
+	AlphaIcon = { fg = colors.base0E, bg = colors.background },
+	AlphaKeyPrefix = { fg = colors.red, bg = utils.blend(colors.red, colors.base00, 0.1) },
+	AlphaMessage = { fg = colors.base0D, bg = colors.background },
+	AlphaFooter = { fg = colors.base0A, bg = colors.background },
 }
