89736f131f04e6e4d8f85df231a7160f647286bf
Alex Rosas <rosasrias82@gmail.com>
2024-12-22 16:45:45 -0500

    Change colors

--- lua/theme/integrations/buf.lua
+++ lua/theme/integrations/buf.lua
@@ -7,15 +7,15 @@ end
 
 return {
     BufflineBufOnActive = { fg = colors.foreground, bg = colors.background },
-    BufflineBufOnInactive = { fg = utils.blend(colors.foreground, colors.lighter, 0.45), bg = utils.blend(colors.lighter, colors.foreground, 0.93) },
+    BufflineBufOnInactive = { fg = utils.blend(colors.foreground, colors.background, 0.45), bg = utils.blend(colors.background, colors.foreground, 0.93) },
     BufflineBufOnModified = { fg = colors.base0D, bg = colors.background },
-    BuffLineBufOffModified = { fg = colors.base0D, bg = utils.blend(colors.lighter, colors.foreground, 0.93) },
+    BuffLineBufOffModified = { fg = colors.base0D, bg = utils.blend(colors.background, colors.foreground, 0.93) },
     BufflineBufOnClose = { fg = colors.base08, bg = colors.background },
-    BuffLineBufOffClose = { fg = colors.base08, bg = utils.blend(colors.lighter, colors.foreground, 0.93) },
+    BuffLineBufOffClose = { fg = colors.base08, bg = utils.blend(colors.background, colors.foreground, 0.93) },
 
-    BuffLineTree = { bg = colors.background, fg = colors.foreground },
+    BuffLineTree = { bg = colors.darker, fg = colors.foreground },
     BuffLineEmpty = { bg = colors.background, fg = colors.foreground },
-    BuffLineEmptyColor = { bg = colors.lighter, fg = colors.foreground },
+    BuffLineEmptyColor = { bg = colors.background, fg = colors.foreground },
     BuffLineButton = { bg = utils.blend(colors.base0E, colors.base00, 0.1), fg = colors.base0E },
     BuffLineCloseButton = { bg = colors.base08, fg = colors.base00 },
     BuffLineRun = { bg = utils.blend(colors.base0F, colors.base00, 0.1), fg = colors.base0F },
