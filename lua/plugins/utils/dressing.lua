0957c78287398e0028a0ff35c51a4e51dfca4c7a
Alex Rosas <rosasrias82@gmail.com>
2024-12-22 16:28:16 -0500

    Add config dressing plugin

--- lua/plugins/utils/dressing.lua
+++ lua/plugins/utils/dressing.lua
@@ -1,4 +1,15 @@
 return {
     'stevearc/dressing.nvim',
-    opts = {},
+    opts = {
+        border = 'solid',
+        input = {
+            enabled = true,
+            border = 'solid',
+            default_prompt = 'Input:',
+            title_pos = 'center',
+        },
+        win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder,Title:Title",
+        },
+    },
 }
