54c42dcd81a4bb8352daf4b8a964243f0aa9bcbd
Alex Rosas <rosasrias82@gmail.com>
2025-01-03 18:27:03 -0500

    Fix format

--- lua/plugins/ts/treesitter.lua
+++ lua/plugins/ts/treesitter.lua
@@ -1,17 +1,16 @@
-return
-{
-    'nvim-treesitter/nvim-treesitter',
-    run = 'TSUpdate',
-    cmd = { 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'python', 'javascript', 'lua' },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	run = "TSUpdate",
+	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "python", "javascript", "lua" },
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
