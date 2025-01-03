a65937810c3e892cdf038478f64e229ef15c187f
Alex Rosas <rosasrias82@gmail.com>
2025-01-03 18:44:30 -0500

    style(formatting): update code indentation to 2 spaces
    Reformatted all files to use 2 spaces instead of 4 for indentation.This
    improves consistency across the project and adheres to the coding style
    guidelines.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,44 +1,44 @@
 local function clone_lazy_nvim_repo()
-    local lazy_plugin_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+	local lazy_plugin_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-    if vim.loop.fs_stat(lazy_plugin_path) then
-        return
-    end
+	if vim.loop.fs_stat(lazy_plugin_path) then
+		return
+	end
 
-    local git_clone_cmd = { 'git', 'clone', 'https://github.com/folke/lazy.nvim.git', lazy_plugin_path }
-    local result = vim.fn.system(git_clone_cmd)
+	local git_clone_cmd = { "git", "clone", "https://github.com/folke/lazy.nvim.git", lazy_plugin_path }
+	local result = vim.fn.system(git_clone_cmd)
 
-    if vim.v.shell_error ~= 0 then
-        error("Error al clonar el repositorio lazy.nvim: " .. result)
-    end
+	if vim.v.shell_error ~= 0 then
+		error("Error al clonar el repositorio lazy.nvim: " .. result)
+	end
 end
 
 function Bootstrap()
-    clone_lazy_nvim_repo()
+	clone_lazy_nvim_repo()
 
-    local lazy_plugin_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-    vim.opt.rtp:prepend(lazy_plugin_path)
+	local lazy_plugin_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	vim.opt.rtp:prepend(lazy_plugin_path)
 end
 
 function Setup()
-    local status_ok, lazy = pcall(require, 'lazy')
+	local status_ok, lazy = pcall(require, "lazy")
 
-    if not status_ok then
-        error("No se pudo cargar el módulo 'lazy'")
-    end
+	if not status_ok then
+		error("No se pudo cargar el módulo 'lazy'")
+	end
 
-    lazy.setup({
-        spec = {
-            { import = 'plugins.utils' },
-            { import = 'plugins.lsp' },
-            { import = 'plugins.comment' },
-            { import = 'plugins.dap' },
-            { import = 'plugins.git' },
-            { import = 'plugins.ia' },
-            { import = 'plugins.ts' },
-            { import = 'plugins.utils' }
-        },
-    })
+	lazy.setup({
+		spec = {
+			{ import = "plugins.lsp" },
+			{ import = "plugins.comment" },
+			{ import = "plugins.dap" },
+			{ import = "plugins.git" },
+			{ import = "plugins.ia" },
+			{ import = "plugins.ts" },
+			{ import = "plugins.utils" },
+			{ import = "plugins.learn" },
+		},
+	})
 end
 
 Bootstrap()
