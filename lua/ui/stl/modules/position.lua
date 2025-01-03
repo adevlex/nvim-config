fe5c9317e77e0e04b7a132aa087285a16b7dbe60
Alex Rosas <rosasrias82@gmail.com>
2025-01-03 18:53:16 -0500

    style(staline): change icon nix for archcraft and formatting style code

--- lua/ui/stl/modules/position.lua
+++ lua/ui/stl/modules/position.lua
@@ -4,21 +4,45 @@ local modes = require("ui.stl.modes")
 local api = vim.api
 
 local M = function(m)
-    local mode = api.nvim_get_mode().mode
-    local current = fn.line "."
-    local total = fn.line "$"
-    if (m == 'blocks') then
-        return "%#StalineProgress#" ..
-            " " .. current .. "/" .. total .. " " .. "%#StalineProgressIcon#" .. " " .. "%#StalineEmptySpace#"
-    elseif (m == 'minimal') then
-        return " %#StalineNix#   " .. "%#StalineProgressMinimal#" ..
-            " " .. current .. "/" .. total .. " " .. "%#" .. modes[mode][2] .. "#" .. " " .. "%#StalineEmptySpace#"
-    elseif (m == 'fancy') then
-        return "%#StalineProgressIcon#" ..
-            "   " .. "%#StalineProgress#" .. " " .. current .. "/" .. total .. " " .. "%#StalineEmptySpace#"
-    else
-        return "F"
-    end
+	local mode = api.nvim_get_mode().mode
+	local current = fn.line(".")
+	local total = fn.line("$")
+	if m == "blocks" then
+		return "%#StalineProgress#"
+			.. " "
+			.. current
+			.. "/"
+			.. total
+			.. " "
+			.. "%#StalineProgressIcon#"
+			.. " "
+			.. "%#StalineEmptySpace#"
+	elseif m == "minimal" then
+		return " %#StalineNix#   "
+			.. "%#StalineProgressMinimal#"
+			.. " "
+			.. current
+			.. "/"
+			.. total
+			.. " "
+			.. "%#"
+			.. modes[mode][2]
+			.. "#"
+			.. " "
+			.. "%#StalineEmptySpace#"
+	elseif m == "fancy" then
+		return "%#StalineProgressIcon#"
+			.. "   "
+			.. "%#StalineProgress#"
+			.. " "
+			.. current
+			.. "/"
+			.. total
+			.. " "
+			.. "%#StalineEmptySpace#"
+	else
+		return "F"
+	end
 end
 
 return M
