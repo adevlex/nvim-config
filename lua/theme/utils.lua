399fc9bfa01fcf19750bef7cddbcdbefab13c5b1
Alex Rosas <rosasrias82@gmail.com>
2025-01-03 19:08:51 -0500

    feat(theme.utils): add theme filtering and reloading utilities
    - Implemented `M.filter` to filter a list of words based on a string
    match, prioritizing exact matches before partial ones.
    - Added `M.reload_theme` to reload a theme by updating `nvimTheme`,
    reloading highlights, and refreshing Volt's highlight module.
    - Used `plenary.reload` to ensure proper reloading of required modules
    after a theme change.
    
    These functions improve the handling of theme selection and reloading
    within the theme system.

---
+++ lua/theme/utils.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+M.filter = function(tb, str)
+	local strlen = #str
+	local result = {}
+
+	for _, word in ipairs(tb) do
+		if str == word:sub(1, strlen) then
+			table.insert(result, word)
+		end
+	end
+
+	for _, word in ipairs(tb) do
+		if string.find(word, str) and not vim.tbl_contains(result, word) then
+			table.insert(result, word)
+		end
+	end
+
+	return #result == 0 and tb or result
+end
+
+M.reload_theme = function(name)
+	vim.g.nvimTheme = name
+	require("theme").load_all_highlights()
+	require("plenary.reload").reload_module("volt.highlights")
+	require("volt.highlights")
+end
+
+return M
