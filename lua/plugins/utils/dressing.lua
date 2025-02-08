d5659d6ad1159be6be977a4891295e25d656e447
Alex Rosas <rosasrias82@gmail.com>
2025-02-07 21:53:05 -0500

    feat: change default config dressing

--- lua/plugins/utils/dressing.lua
+++ lua/plugins/utils/dressing.lua
@@ -1,15 +1,15 @@
 return {
-    'stevearc/dressing.nvim',
-    opts = {
-        border = 'solid',
-        input = {
-            enabled = true,
-            border = 'solid',
-            default_prompt = 'Input:',
-            title_pos = 'center',
-        },
-        win_options = {
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder,Title:Title",
-        },
-    },
+	"stevearc/dressing.nvim",
+	opts = {
+		border = vim.g.transparency and "rounded" or "solid",
+		input = {
+			enabled = true,
+			border = vim.g.transparency and "rounded" or "solid",
+			default_prompt = "Input:",
+			title_pos = "center",
+		},
+		win_options = {
+			winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder,Title:Title",
+		},
+	},
 }
